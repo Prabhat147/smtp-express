7481720befc711c584ddf940fcf212a953dd7a3a
Prabhat147 <prabhat.ap21@gmail.com>
2024-09-25 15:54:45 +0545

    smtp setup

---
+++ Medicare Express/Program.cs
@@ -0,0 +1,21 @@
+﻿using Medicare_Express;
+using System;
+using System.Windows.Forms;
+
+namespace EmailSenderApp
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new express());
+        }
+    }
+
+    internal class MainForm : Form
+    {
+    }
+}
